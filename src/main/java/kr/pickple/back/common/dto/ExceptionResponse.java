e2940f50a69c0858daa6fbb1d699bc271f3f8718
changhyeon <changhyeon.h@kakao.com>
2023-10-24 09:42:20 +0900

    refactor: Builder 클래스로 위치 변경, Lombok 사용

--- src/main/java/kr/pickple/back/common/dto/ExceptionResponse.java
+++ src/main/java/kr/pickple/back/common/dto/ExceptionResponse.java
@@ -1,19 +1,16 @@
 package kr.pickple.back.common.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExceptionResponse {
 
     private final String code;
     private final String message;
     private final Object[] rejectedValues;
-
-    @Builder
-    protected ExceptionResponse(String code, String message, Object[] rejectedValues) {
-        this.code = code;
-        this.message = message;
-        this.rejectedValues = rejectedValues;
-    }
 }
