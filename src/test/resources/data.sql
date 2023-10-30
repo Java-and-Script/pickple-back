cf5934a7b800fc0d41fb0bf9a4bd5a875b271aee
kylekim2123 <kyle2123@naver.com>
2023-10-31 01:06:15 +0900

    test: 지역 목록 조회, 주 활동 지역 조회 기능에 대한 테스트 코드 작성

---
+++ src/test/resources/data.sql
@@ -0,0 +1,28 @@
+-- Address 정보 삽입
+INSERT INTO address_depth1 (name) VALUES ('서울시');
+
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('강남구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('강동구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('강북구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('강서구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('관악구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('광진구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('구로구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('금천구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('노원구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('도봉구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('동대문구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('동작구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('마포구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('서대문구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('서초구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('성동구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('성북구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('송파구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('양천구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('영등포구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('용산구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('은평구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('종로구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('중구', 1);
+INSERT INTO address_depth2 (name, address_depth1_id) VALUES ('중랑구', 1);
