6b9a862a7e54a1434e3e29cd67b013e0325ed370
kylekim2123 <kyle2123@naver.com>
2023-10-31 00:24:59 +0900

    style: 카멜 케이스 이슈로 인한 address 테이블 명 변경

--- src/main/java/kr/pickple/back/address/domain/AddressDepth2.java
+++ src/main/java/kr/pickple/back/address/domain/AddressDepth2.java
@@ -2,9 +2,12 @@ package kr.pickple.back.address.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import kr.pickple.back.common.domain.BaseEntity;
 import lombok.AccessLevel;
@@ -21,4 +24,9 @@ public class AddressDepth2 extends BaseEntity {
     @NotNull
     @Column(unique = true, length = 10)
     private String name;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_depth1_id")
+    private AddressDepth1 addressDepth1;
 }
