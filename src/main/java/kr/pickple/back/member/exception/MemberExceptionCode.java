3dc39f754c914e03b1641329ec602d8d2f9b0dbd
changhyeon <changhyeon.h@kakao.com>
2023-10-31 17:12:28 +0900

    feat: Member 예외 처리 구현

---
+++ src/main/java/kr/pickple/back/member/exception/MemberExceptionCode.java
@@ -0,0 +1,20 @@
+package kr.pickple.back.member.exception;
+
+import org.springframework.http.HttpStatus;
+
+import kr.pickple.back.common.exception.ExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberExceptionCode implements ExceptionCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEM-001", "사용자를 찾을 수 없음"),
+    MEMBER_IS_EXISTED(HttpStatus.BAD_REQUEST, "MEM-002", "이미 존재하는 사용자 정보"),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
