ee9add534225b2e925fb8143c29534499a73f5c4
changhyeon <changhyeon.h@kakao.com>
2023-10-31 22:21:31 +0900

    refactor: url은 application.yml파일로 분리

--- src/main/java/kr/pickple/back/auth/config/AuthConfig.java
+++ src/main/java/kr/pickple/back/auth/config/AuthConfig.java
@@ -3,10 +3,11 @@ package kr.pickple.back.auth.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import kr.pickple.back.auth.config.property.CorsConfig;
 import kr.pickple.back.auth.config.property.KakaoOAuthConfig;
 
 @Configuration
-@EnableConfigurationProperties(value = {KakaoOAuthConfig.class})
+@EnableConfigurationProperties(value = {KakaoOAuthConfig.class, CorsConfig.class})
 public class AuthConfig {
 
 }
