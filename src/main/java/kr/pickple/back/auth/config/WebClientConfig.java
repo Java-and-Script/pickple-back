ef9ea20ec50d3434881a8ae77646af0a07d2a6f0
changhyeon <changhyeon.h@kakao.com>
2023-11-01 02:37:22 +0900

    refactor: WebClientConfig Util 클래스로 변경

--- src/main/java/kr/pickple/back/auth/config/WebClientConfig.java
+++ src/main/java/kr/pickple/back/auth/config/WebClientConfig.java
@@ -3,19 +3,19 @@ package kr.pickple.back.auth.config;
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.*;
 import static org.springframework.http.MediaType.*;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Configuration
-public class WebClientConfig {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-    @Bean
-    public ExchangeStrategies getExchangeStrategies() {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class WebClientConfig {
+
+    public static ExchangeStrategies getExchangeStrategies() {
         ObjectMapper objectMapper = new ObjectMapper()
                 .setPropertyNamingStrategy(SNAKE_CASE)
                 .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
