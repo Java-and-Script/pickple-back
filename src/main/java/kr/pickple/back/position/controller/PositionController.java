ce45681360c3d33994744dc71aff11136ec016a2
charlesuu <tiger0192004@gmail.com>
2023-10-31 15:54:00 +0900

    feat: 포지션 목록 조회 기능 구현

---
+++ src/main/java/kr/pickple/back/position/controller/PositionController.java
@@ -0,0 +1,25 @@
+package kr.pickple.back.position.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.pickple.back.position.domain.Position;
+import kr.pickple.back.position.dto.PositionResponse;
+import kr.pickple.back.position.service.PositionService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/positions")
+@RequiredArgsConstructor
+public class PositionController {
+    private final PositionService positionService;
+
+    @GetMapping
+    public List<PositionResponse> findAllPositions() {
+        return positionService.findAllPositions();
+    }
+}
