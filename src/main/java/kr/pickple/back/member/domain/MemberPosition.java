857ae601ff5c7b17630de4693e03f345332c73ec
changhyeon <changhyeon.h@kakao.com>
2023-10-31 17:12:55 +0900

    feat: Member Position 저장 기능 구현

--- src/main/java/kr/pickple/back/member/domain/MemberPosition.java
+++ src/main/java/kr/pickple/back/member/domain/MemberPosition.java
@@ -1,9 +1,8 @@
 package kr.pickple.back.member.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,7 +12,9 @@ import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import kr.pickple.back.common.domain.BaseEntity;
 import kr.pickple.back.position.domain.Position;
+import kr.pickple.back.position.domain.Position.PositionConverter;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -25,11 +26,17 @@ public class MemberPosition extends BaseEntity {
     private Long id;
 
     @NotNull
-    @Enumerated(value = EnumType.STRING)
+    @Convert(converter = PositionConverter.class)
     @Column(length = 2)
     private Position position;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder
+    private MemberPosition(final Position position, final Member member) {
+        this.position = position;
+        this.member = member;
+    }
 }
