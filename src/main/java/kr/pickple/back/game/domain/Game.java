0d3e5b43dec9c4ddb7b5463ba941cc8d42da0e8d
charlesuu <tiger0192004@gmail.com>
2023-11-02 01:13:51 +0900

    feat: 게임 관련 각 도메인에 빌더 추가

--- src/main/java/kr/pickple/back/game/domain/Game.java
+++ src/main/java/kr/pickple/back/game/domain/Game.java
@@ -19,9 +19,12 @@ import kr.pickple.back.address.domain.AddressDepth2;
 import kr.pickple.back.common.domain.BaseEntity;
 import kr.pickple.back.member.domain.Member;
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Game extends BaseEntity {
 
@@ -59,8 +62,9 @@ public class Game extends BaseEntity {
     @NotNull
     @Column(length = 10)
     @Enumerated(value = EnumType.STRING)
-    private GameStatus status;
+    private GameStatus status = GameStatus.OPEN;
 
+    //todo 현호: 게시글 상세 조회 기능 구현시 viewCount 올리는 기능 구현
     @NotNull
     private Integer viewCount = 0;
 
@@ -87,4 +91,34 @@ public class Game extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "address_depth2_id")
     private AddressDepth2 addressDepth2;
+
+    @Builder
+    private Game(
+            final String content,
+            final LocalDate playDate,
+            final LocalTime playStartTime,
+            final LocalTime playEndTime,
+            final Integer playTimeMinutes,
+            final String mainAddress,
+            final String detailAddress,
+            final GameStatus status,
+            final Integer cost,
+            final Integer maxMemberCount,
+            final Member host,
+            final AddressDepth1 addressDepth1,
+            final AddressDepth2 addressDepth2
+    ) {
+        this.content = content;
+        this.playDate = playDate;
+        this.playStartTime = playStartTime;
+        this.playEndTime = playEndTime;
+        this.playTimeMinutes = playTimeMinutes;
+        this.mainAddress = mainAddress;
+        this.detailAddress = detailAddress;
+        this.cost = cost;
+        this.maxMemberCount = maxMemberCount;
+        this.host = host;
+        this.addressDepth1 = addressDepth1;
+        this.addressDepth2 = addressDepth2;
+    }
 }
