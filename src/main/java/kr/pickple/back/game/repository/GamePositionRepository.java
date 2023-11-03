970ab8ae9a73f545d97a116e59ee529576531e42
charlesuu <tiger0192004@gmail.com>
2023-11-03 14:53:58 +0900

    refactor: Game, GamePosition 간 양방향 연관관계 부여, GamePositionRepository제거

--- src/main/java/kr/pickple/back/game/repository/GamePositionRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.pickple.back.game.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kr.pickple.back.game.domain.GamePosition;
-
-public interface GamePositionRepository extends JpaRepository<GamePosition, Long> {
-    
-}
