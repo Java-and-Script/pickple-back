60ac13eb7d8d7fadfd65c8d841cd4bab34a3159a
jay-so <merry9504@naver.com>
2023-11-03 17:35:32 +0900

    refactor: RegistrationStatus(크루원 가입 상태) 역직렬화, 직렬화 설정 추가 리팩토링

--- src/main/java/kr/pickple/back/common/domain/RegistrationStatus.java
+++ src/main/java/kr/pickple/back/common/domain/RegistrationStatus.java
@@ -1,7 +1,18 @@
 package kr.pickple.back.common.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import kr.pickple.back.crew.exception.CrewException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static kr.pickple.back.crew.exception.CrewExceptionCode.CREW_MEMBER_STATUS_NOT_FOUND;
 
 @Getter
 @RequiredArgsConstructor
@@ -11,5 +22,17 @@ public enum RegistrationStatus {
     CONFIRMED("확정"),
     ;
 
+    private static final Map<String, RegistrationStatus> registrationStatusMap = Collections.unmodifiableMap(Stream.of(values())
+            .collect(Collectors.toMap(RegistrationStatus::getDescription, Function.identity())));
+
+    @JsonValue
     private final String description;
+
+    @JsonCreator
+    public static RegistrationStatus from(final String description) {
+        if (registrationStatusMap.containsKey(description)) {
+            return registrationStatusMap.get(description);
+        }
+        throw new CrewException(CREW_MEMBER_STATUS_NOT_FOUND, description);
+    }
 }
