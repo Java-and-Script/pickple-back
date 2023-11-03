179b3955a7d3c9ee59bde3059da186ec4e2192df
jay-so <merry9504@naver.com>
2023-11-03 23:23:34 +0900

    refactor: CrewExceptionCode 수정

--- src/main/java/kr/pickple/back/crew/exception/CrewExceptionCode.java
+++ src/main/java/kr/pickple/back/crew/exception/CrewExceptionCode.java
@@ -12,8 +12,9 @@ public enum CrewExceptionCode implements ExceptionCode {
     CREW_NOT_FOUND(HttpStatus.NOT_FOUND, "CRE-001", "크루를 찾을 수 없음"),
     CREW_IS_EXISTED(HttpStatus.BAD_REQUEST, "CRE-002", "이미 존재하는 크루 정보"),
     CREW_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "CRE-003", "크루의 상태는 모집 중이거나 모집 마감만 가능"),
-    CREW_MEMBER_ALREADY_JOINED(HttpStatus.BAD_REQUEST, "CRE-004", "이미 가입된 크루"),
-    CREW_MEMBER_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "CRE-004", "크루 가입자의 상태는 대기나 확정만 가능");;
+    CREW_MEMBER_ALREADY_EXISTED(HttpStatus.BAD_REQUEST, "CRE-004", "이미 가입 신청이 되거나 가입된 크루"),
+    CREW_MEMBER_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "CRE-005", "크루 가입자의 상태는 대기나 확정만 가능"),
+    ;
 
     private final HttpStatus status;
     private final String code;
