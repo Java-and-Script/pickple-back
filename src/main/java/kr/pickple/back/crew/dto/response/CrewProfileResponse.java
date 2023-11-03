32cc32798e19fd5ef87d280ae5be8f8b910c7a49
jay-so <merry9504@naver.com>
2023-11-03 14:17:04 +0900

    feat: Crew 상세 조회 기능 구현

---
+++ src/main/java/kr/pickple/back/crew/dto/response/CrewProfileResponse.java
@@ -0,0 +1,66 @@
+package kr.pickple.back.crew.dto.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import kr.pickple.back.crew.domain.Crew;
+import kr.pickple.back.crew.domain.CrewStatus;
+import kr.pickple.back.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CrewProfileResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String content;
+
+    private Integer memberCount;
+
+    private Integer maxMemberCount;
+
+    private String profileImageUrl;
+
+    private String backgroundImageUrl;
+
+    private CrewStatus status;
+
+    private Integer likeCount;
+
+    private Integer competitionPoint;
+
+    private Member leader;
+
+    private String addressDepth1;
+
+    private String addressDepth2;
+
+    private List<Member> members;
+
+    public static CrewProfileResponse fromEntity(final Crew crew) {
+        return CrewProfileResponse.builder()
+                .id(crew.getId())
+                .name(crew.getName())
+                .content(crew.getContent())
+                .memberCount(crew.getMemberCount())
+                .maxMemberCount(crew.getMaxMemberCount())
+                .profileImageUrl(crew.getProfileImageUrl())
+                .backgroundImageUrl(crew.getBackgroundImageUrl())
+                .status(crew.getStatus())
+                .likeCount(crew.getLikeCount())
+                .competitionPoint(crew.getCompetitionPoint())
+                .leader(Member.builder().build())
+                .addressDepth1(crew.getAddressDepth1().getName())
+                .addressDepth2(crew.getAddressDepth2().getName())
+                //TODO:추후 Member 도메인 완성되면 추가(11월 1일, 소재훈)
+                .build();
+    }
+}
