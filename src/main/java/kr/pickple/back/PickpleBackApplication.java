7cf5a7eba9b338a4692ebd2d22fddac1dc6c3d27
jay-so <merry9504@naver.com>
2023-11-03 18:24:23 +0900

    refactor: S3 properties 클래스 분리, S3 Config 클래스 리팩토링

--- src/main/java/kr/pickple/back/PickpleBackApplication.java
+++ src/main/java/kr/pickple/back/PickpleBackApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(S3Config.class)
 public class PickpleBackApplication {
 
     public static void main(String[] args) {
--- src/main/java/kr/pickple/back/common/config/S3Config.java
+++ src/main/java/kr/pickple/back/common/config/S3Config.java
@@ -1,23 +1,11 @@
 package kr.pickple.back.common.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import kr.pickple.back.common.config.property.S3Properties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties(prefix = "s3.default")
+@Configuration
+@EnableConfigurationProperties(value = S3Properties.class)
 public class S3Config {
 
-    private final String profile;
-    private final String background;
-
-    public S3Config(String profile, String background) {
-        this.profile = profile;
-        this.background = background;
-    }
-
-    public String getProfile() {
-        return profile;
-    }
-
-    public String getBackground() {
-        return background;
-    }
 }
