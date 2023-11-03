9339616193e1df5babd8c991168e65cd81e01162
jay-so <merry9504@naver.com>
2023-11-03 14:30:22 +0900

    refactor: CrewRepository 리팩토링

--- src/main/java/kr/pickple/back/crew/repository/CrewRepository.java
+++ src/main/java/kr/pickple/back/crew/repository/CrewRepository.java
@@ -3,11 +3,7 @@ package kr.pickple.back.crew.repository;
 import kr.pickple.back.crew.domain.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 
     boolean existsByName(String name);
-
-    Optional<Crew> findCrewById(Long id);
 }
