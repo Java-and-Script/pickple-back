97528f505be48c8fa4cb27adfec2c6440c74f1ac
jay-so <merry9504@naver.com>
2023-11-07 05:56:43 +0900

    feat: 사용자 근처 크루 목록 조회 기능 구현

--- src/main/java/kr/pickple/back/crew/repository/CrewRepository.java
+++ src/main/java/kr/pickple/back/crew/repository/CrewRepository.java
@@ -1,9 +1,15 @@
 package kr.pickple.back.crew.repository;
 
+import kr.pickple.back.address.domain.AddressDepth1;
+import kr.pickple.back.address.domain.AddressDepth2;
 import kr.pickple.back.crew.domain.Crew;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 
     boolean existsByName(final String name);
+
+    Page<Crew> findByAddressDepth1AndAddressDepth2(final AddressDepth1 addressDepth1, final AddressDepth2 addressDepth2, final Pageable pageable);
 }
--- src/main/java/kr/pickple/back/crew/service/CrewService.java
+++ src/main/java/kr/pickple/back/crew/service/CrewService.java
@@ -1,9 +1,13 @@
 package kr.pickple.back.crew.service;
 
+import kr.pickple.back.address.domain.AddressDepth1;
+import kr.pickple.back.address.domain.AddressDepth2;
 import kr.pickple.back.address.dto.response.MainAddressResponse;
 import kr.pickple.back.address.service.AddressService;
 import kr.pickple.back.common.config.property.S3Properties;
+import kr.pickple.back.common.domain.RegistrationStatus;
 import kr.pickple.back.crew.domain.Crew;
+import kr.pickple.back.crew.domain.CrewMember;
 import kr.pickple.back.crew.dto.request.CrewCreateRequest;
 import kr.pickple.back.crew.dto.response.CrewIdResponse;
 import kr.pickple.back.crew.dto.response.CrewProfileResponse;
@@ -15,10 +19,14 @@ import kr.pickple.back.member.dto.response.MemberResponse;
 import kr.pickple.back.member.exception.MemberException;
 import kr.pickple.back.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static kr.pickple.back.common.domain.RegistrationStatus.CONFIRMED;
 import static kr.pickple.back.crew.exception.CrewExceptionCode.CREW_IS_EXISTED;
@@ -64,6 +72,24 @@ public class CrewService {
                 .toList();
 
         return CrewProfileResponse.fromEntity(crew, crewMembers);
+    }
+
+    public List<CrewProfileResponse> findCrewByAddress(final String addressDepth1, final String addressDepth2, final int page, final int size) {
+        final AddressDepth1 depth1 = addressService.findByExistAddressDepth1(addressDepth1);
+        final AddressDepth2 depth2 = addressService.findByExistAddressDepth2(addressDepth2, depth1);
+
+        final Pageable pageable = PageRequest.of(page - 1, size);
+        final Page<Crew> crewPage = crewRepository.findByAddressDepth1AndAddressDepth2(depth1, depth2, pageable);
+
+        return crewPage.stream()
+                .map(crew -> {
+                    final List<CrewMember> crewMemberList = crewMemberRepository.findCrewMemberByStatusAndCrewId(RegistrationStatus.CONFIRMED, crew.getId());
+                    final List<CrewMemberRelationDto> crewMembers = crewMemberList.stream()
+                            .map(CrewMemberRelationDto::fromEntity)
+                            .collect(Collectors.toList());
+                    return CrewProfileResponse.fromEntity(crew, crewMembers);
+                })
+                .collect(Collectors.toList());
     }
 
     private void validateIsDuplicatedCrewInfo(final String name) {
