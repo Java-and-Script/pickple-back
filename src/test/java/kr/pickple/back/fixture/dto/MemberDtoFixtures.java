46985474b0eccc50a22b2f3e65bb63da03018200
changhyeon <changhyeon.h@kakao.com>
2023-11-07 10:43:19 +0900

    feat: oauth Document Test 적용

--- src/test/java/kr/pickple/back/fixture/dto/MemberDtoFixtures.java
+++ src/test/java/kr/pickple/back/fixture/dto/MemberDtoFixtures.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import kr.pickple.back.auth.domain.oauth.OauthProvider;
 import kr.pickple.back.member.dto.request.MemberCreateRequest;
+import kr.pickple.back.member.dto.response.AuthenticatedMemberResponse;
 
 public class MemberDtoFixtures {
 
@@ -17,6 +18,32 @@ public class MemberDtoFixtures {
                 .oauthProvider(OauthProvider.KAKAO)
                 .addressDepth1("서울시")
                 .addressDepth2("영등포구")
+                .build();
+    }
+
+    public static AuthenticatedMemberResponse authenticatedMemberResponseLoginBuild() {
+        return AuthenticatedMemberResponse.builder()
+                .accessToken("accessToken")
+                .refreshToken("refreshToken")
+                .id(1L)
+                .nickname("강백둥")
+                .profileImageUrl("https://amazon.image/1")
+                .email("pickple1@pickple.kr")
+                .oauthId(1L)
+                .oauthProvider(OauthProvider.KAKAO)
+                .addressDepth1("서울시")
+                .addressDepth2("영등포구")
+                .build();
+    }
+
+    public static AuthenticatedMemberResponse authenticatedMemberResponseRegistrationBuild() {
+        return AuthenticatedMemberResponse.builder()
+                .accessToken("accessToken")
+                .nickname("강백둥")
+                .profileImageUrl("https://amazon.image/1")
+                .email("pickple1@pickple.kr")
+                .oauthId(1L)
+                .oauthProvider(OauthProvider.KAKAO)
                 .build();
     }
 }
