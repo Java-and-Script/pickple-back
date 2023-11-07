260bdf704a13c1e09d95e2389cfb2cdeb1b90c13
changhyeon <changhyeon.h@kakao.com>
2023-11-07 22:05:48 +0900

    style: 변수명 수정

--- src/main/java/kr/pickple/back/member/domain/MemberCrews.java
+++ src/main/java/kr/pickple/back/member/domain/MemberCrews.java
@@ -14,17 +14,17 @@ import kr.pickple.back.crew.domain.CrewMember;
 public class MemberCrews {
 
     @OneToMany(mappedBy = "member", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
-    private List<CrewMember> crewMembers = new ArrayList<>();
+    private List<CrewMember> memberCrews = new ArrayList<>();
 
     public List<Crew> getCrewsByStatus(final RegistrationStatus status) {
-        return crewMembers.stream()
+        return memberCrews.stream()
                 .filter(crewMember -> crewMember.equalsStatus(status))
                 .map(CrewMember::getCrew)
                 .toList();
     }
 
     public List<Crew> getCreatedCrewsByMember(final Member member) {
-        return crewMembers.stream()
+        return memberCrews.stream()
                 .map(CrewMember::getCrew)
                 .filter(crew -> crew.isLeader(member))
                 .toList();
