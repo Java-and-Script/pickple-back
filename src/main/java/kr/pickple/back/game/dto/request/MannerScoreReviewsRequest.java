af2de5f9412adea8ef97677ffd1c1ae8e6bff734
kylekim2123 <kyle2123@naver.com>
2023-11-12 05:39:59 +0900

    refactor: 게스트 모집 관련 Test Fixture를 만들기 위한 builder, getter 추가

--- src/main/java/kr/pickple/back/game/dto/request/MannerScoreReviewsRequest.java
+++ src/main/java/kr/pickple/back/game/dto/request/MannerScoreReviewsRequest.java
@@ -5,11 +5,15 @@ import java.util.List;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MannerScoreReviewsRequest {
 
     @Valid
