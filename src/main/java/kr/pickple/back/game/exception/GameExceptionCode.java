6a406d76fa68301c217a5457f7f8ca240c868663
kylekim2123 <kyle2123@naver.com>
2023-11-11 20:51:22 +0900

    feat: 게스트 모집 관련 인가 처리 추가 및 매너 스코어 리뷰 예외처리 추가

--- src/main/java/kr/pickple/back/game/exception/GameExceptionCode.java
+++ src/main/java/kr/pickple/back/game/exception/GameExceptionCode.java
@@ -15,7 +15,13 @@ public enum GameExceptionCode implements ExceptionCode {
     GAME_MEMBER_IS_EXISTED(HttpStatus.BAD_REQUEST, "GAM-003", "이미 해당 게스트 모집글에 참여 신청한 사용자"),
     GAME_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "GAM-004", "게스트 모집글에 참여 신청된 혹은 확정된 사용자들 중에서 해당 사용자를 찾을 수 없음"),
     GAME_POSITIONS_IS_DUPLICATED(HttpStatus.BAD_REQUEST, "GAM-005", "게스트 모집글의 포지션 목록에 중복이 존재함"),
-    GAME_SEARCH_CATEGORY_IS_INVALID(HttpStatus.BAD_REQUEST, "GAM-006", "게스트 모집글의 검색 카테고리 키워드가 유효하지 않음");
+    GAME_SEARCH_CATEGORY_IS_INVALID(HttpStatus.BAD_REQUEST, "GAM-006", "게스트 모집글의 검색 카테고리 키워드가 유효하지 않음"),
+    GAME_MEMBER_IS_NOT_HOST(HttpStatus.FORBIDDEN, "GAM-007", "해당 게스트 모집글의 호스트 권한이 필요함"),
+    GAME_HOST_CANNOT_BE_DELETED(HttpStatus.BAD_REQUEST, "GAM-008", "호스트는 자신의 게스트 모집글에서 삭제될 수 없음"),
+    GAME_MEMBER_STATUS_IS_NOT_WAITING(HttpStatus.BAD_REQUEST, "GAM-009", "해당 게스트 모집글에 참여 신청 대기 상태가 아니라면, 참여 신청을 취소할 수 없음"),
+    GAME_NOT_ALLOWED_TO_DELETE_GAME_MEMBER(HttpStatus.FORBIDDEN, "GAM-010", "해당 사용자의 게스트 모집글 참여 신청을 거절 혹은 취소할 권한이 필요함"),
+    GAME_MEMBERS_CAN_REVIEW_AFTER_PLAYING(HttpStatus.BAD_REQUEST, "GAM-011", "경기 종료 이전에는 리뷰를 남길 수 없음"),
+    ;
 
     private final HttpStatus status;
     private final String code;
