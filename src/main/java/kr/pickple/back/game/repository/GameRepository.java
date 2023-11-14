a053c760bfd4efc31545a4f8e1cd35955afef8fb
changhyeon <changhyeon.h@kakao.com>
2023-11-14 22:32:16 +0900

    feat: 거리안에 있는 게스트 모집 글 목록 조회 기능 구현

--- src/main/java/kr/pickple/back/game/repository/GameRepository.java
+++ src/main/java/kr/pickple/back/game/repository/GameRepository.java
@@ -1,8 +1,12 @@
 package kr.pickple.back.game.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import kr.pickple.back.address.domain.AddressDepth1;
 import kr.pickple.back.address.domain.AddressDepth2;
@@ -10,6 +14,16 @@ import kr.pickple.back.game.domain.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+    String HAVERSINE_FORMULA = "(6371 * acos(cos(radians(:latitude)) * cos(radians(g.latitude)) *" +
+            " cos(radians(g.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(g.latitude))))";
+
     Page<Game> findByAddressDepth1AndAddressDepth2(final AddressDepth1 addressDepth1, final AddressDepth2 addressDepth2,
             final Pageable pageable);
+
+    @Query("SELECT g FROM Game g WHERE " + HAVERSINE_FORMULA + " < :distance ORDER BY " + HAVERSINE_FORMULA)
+    List<Game> findGamesWithInDistance(
+            @Param("latitude") final Double latitude,
+            @Param("longitude") final Double longitude,
+            @Param("distance") final Double distanceWithInKm
+    );
 }
