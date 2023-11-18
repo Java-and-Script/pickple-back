7b1366fbe016b36e5b61b896c3e4c7fafb7ad82b
changhyeon <changhyeon.h@kakao.com>
2023-11-18 17:48:57 +0900

    feat: JPA Repository랑 QueryDsl Repository 통합

--- src/main/java/kr/pickple/back/game/repository/GameSearchRepository.java
+++ src/main/java/kr/pickple/back/game/repository/GameSearchRepository.java
@@ -1,53 +1,10 @@
 package kr.pickple.back.game.repository;
 
-import static kr.pickple.back.game.domain.QGame.*;
-
 import java.util.List;
 
-import org.springframework.stereotype.Repository;
-
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
 import kr.pickple.back.game.domain.Game;
-import lombok.RequiredArgsConstructor;
 
-@Repository
-@RequiredArgsConstructor
-public class GameSearchRepository {
+public interface GameSearchRepository {
 
-    private final JPAQueryFactory jpaQueryFactory;
-
-    public List<Game> findGamesWithInDistance(
-            final Double latitude,
-            final Double longitude,
-            final Double distance
-    ) {
-        final String pointWKT = String.format("POINT(%s %s)", latitude, longitude);
-
-        return jpaQueryFactory
-                .selectFrom(game)
-                .where(isWithInDistance(pointWKT, distance))
-                .orderBy(getOrderByDistance(pointWKT))
-                .fetch();
-    }
-
-    public BooleanExpression isWithInDistance(final String pointWKT, final Double distance) {
-        return Expressions.booleanTemplate(
-                "ST_Contains(ST_Buffer(ST_GeomFromText({0}, 4326), {1}), point)",
-                pointWKT,
-                distance
-        );
-    }
-
-    public OrderSpecifier<Double> getOrderByDistance(final String pointWKT) {
-        return Expressions.numberTemplate(
-                        Double.class,
-                        "ST_Distance_Sphere(point, ST_GeomFromText({0}, 4326))",
-                        pointWKT
-                )
-                .asc();
-    }
+    List<Game> findGamesWithInDistance(final Double latitude, final Double longitude, final Double distance);
 }
