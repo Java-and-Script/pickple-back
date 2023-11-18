349e1391c8ca05a79e720f55c63af7ca98f3f229
changhyeon <changhyeon.h@kakao.com>
2023-11-18 17:48:57 +0900

    feat: JPA Repository랑 QueryDsl Repository 통합

--- src/main/java/kr/pickple/back/game/repository/GameRepository.java
+++ src/main/java/kr/pickple/back/game/repository/GameRepository.java
@@ -1,29 +1,15 @@
 package kr.pickple.back.game.repository;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import kr.pickple.back.address.domain.AddressDepth1;
 import kr.pickple.back.address.domain.AddressDepth2;
 import kr.pickple.back.game.domain.Game;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<Game, Long>, GameSearchRepository {
 
     Page<Game> findByAddressDepth1AndAddressDepth2(final AddressDepth1 addressDepth1, final AddressDepth2 addressDepth2,
             final Pageable pageable);
-
-    @Query("SELECT g "
-            + "FROM Game g  "
-            + "WHERE ST_Contains(ST_Buffer(ST_GeomFromText(CONCAT('POINT(', :latitude, ' ', :longitude, ')'), 4326), :distance), g.point)"
-            + "ORDER BY ST_Distance_Sphere(g.point, ST_GeomFromText(CONCAT('POINT(', :latitude, ' ', :longitude, ')'), 4326))"
-    )
-    List<Game> findGamesWithInDistance(
-            final Double latitude,
-            final Double longitude,
-            final Double distance
-    );
 }
--- src/main/java/kr/pickple/back/game/service/GameService.java
+++ src/main/java/kr/pickple/back/game/service/GameService.java
@@ -28,7 +28,6 @@ import kr.pickple.back.game.dto.response.GameResponse;
 import kr.pickple.back.game.exception.GameException;
 import kr.pickple.back.game.repository.GameMemberRepository;
 import kr.pickple.back.game.repository.GameRepository;
-import kr.pickple.back.game.repository.GameSearchRepository;
 import kr.pickple.back.member.domain.Member;
 import kr.pickple.back.member.dto.response.MemberResponse;
 import kr.pickple.back.member.exception.MemberException;
@@ -45,7 +44,6 @@ public class GameService {
     private final GameMemberRepository gameMemberRepository;
     private final MemberRepository memberRepository;
     private final KakaoAddressSearchClient kakaoAddressSearchClient;
-    private final GameSearchRepository gameSearchRepository;
 
     @Transactional
     public GameIdResponse createGame(final GameCreateRequest gameCreateRequest, final Long loggedInMemberId) {
@@ -254,7 +252,7 @@ public class GameService {
             final Double longitude,
             final Double distance
     ) {
-        final List<Game> games = gameSearchRepository.findGamesWithInDistance(latitude, longitude, distance);
+        final List<Game> games = gameRepository.findGamesWithInDistance(latitude, longitude, distance);
 
         return games.stream()
                 .map(game -> GameResponse.of(game, getMemberResponses(game, CONFIRMED)))
