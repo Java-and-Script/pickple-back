03b915536fdf58266a23d1c9f9af33f718e91870
jay-so <merry9504@naver.com>
2023-11-19 15:35:17 +0900

    feat:알람 상태, 알람 타입 Converter 구현

---
+++ src/main/java/kr/pickple/back/alaram/util/AlaramTypeConverter.java
@@ -0,0 +1,17 @@
+package kr.pickple.back.alaram.util;
+
+import jakarta.persistence.AttributeConverter;
+import kr.pickple.back.alaram.domain.AlaramType;
+
+public class AlaramTypeConverter  implements AttributeConverter<AlaramType,String> {
+
+    @Override
+    public String convertToDatabaseColumn(final AlaramType alaramType) {
+        return alaramType.getDescription();
+    }
+
+    @Override
+    public AlaramType convertToEntityAttribute(final String alaramType) {
+        return AlaramType.from(alaramType);
+    }
+}
