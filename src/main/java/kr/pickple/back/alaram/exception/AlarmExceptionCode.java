b431b60d5d00330ded18dedc45326ff75821cfcb
jay-so <merry9504@naver.com>
2023-11-21 00:12:58 +0900

    feat: Alarm관련 에러 코드 업데이트

--- src/main/java/kr/pickple/back/alaram/exception/AlarmExceptionCode.java
+++ src/main/java/kr/pickple/back/alaram/exception/AlarmExceptionCode.java
@@ -12,6 +12,7 @@ public enum AlarmExceptionCode implements ExceptionCode {
     ALARM_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-001", "알람 읽음 여부를 찾을 수 없음"),
     ALARM_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-002", "알림 타입을 찾을 수 없음"),
     ALARM_EXISTS_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-003", "사용자의 알람 상태 여부를 찾을 수 없음"),
+    ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-004", "해당 알람을 찾을 수 없음"),
     ;
 
 
