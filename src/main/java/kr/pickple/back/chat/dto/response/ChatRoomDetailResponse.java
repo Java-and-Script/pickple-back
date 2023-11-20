7930102521fd73d5b1e5b5f5dba5f446a8bf58c3
kylekim2123 <kyle2123@naver.com>
2023-11-20 16:20:52 +0900

    feat: 채팅방 요청, 응답 DTO 정의

---
+++ src/main/java/kr/pickple/back/chat/dto/response/ChatRoomDetailResponse.java
@@ -0,0 +1,83 @@
+package kr.pickple.back.chat.dto.response;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import kr.pickple.back.chat.domain.ChatRoom;
+import kr.pickple.back.chat.domain.RoomType;
+import kr.pickple.back.crew.domain.Crew;
+import kr.pickple.back.game.domain.Game;
+import kr.pickple.back.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChatRoomDetailResponse {
+
+    private Long id;
+    private String roomName;
+    private String roomIconImageUrl;
+    private RoomType type;
+    private Long domainId;
+    private Integer memberCount;
+    private Integer maxMemberCount;
+    private LocalTime playStartTime;
+    private Integer playTimeMinutes;
+    private List<ChatMemberResponse> members;
+    private LocalDateTime createdAt;
+
+    public static ChatRoomDetailResponse of(final ChatRoom chatRoom, final Member receiver) {
+        return ChatRoomDetailResponse.builder()
+                .id(chatRoom.getId())
+                .roomName(receiver.getNickname())
+                .roomIconImageUrl(receiver.getProfileImageUrl())
+                .type(chatRoom.getType())
+                .domainId(receiver.getId())
+                .memberCount(chatRoom.getMemberCount())
+                .maxMemberCount(chatRoom.getMaxMemberCount())
+                .members(getChatMemberResponses(chatRoom))
+                .createdAt(chatRoom.getCreatedAt())
+                .build();
+    }
+
+    public static ChatRoomDetailResponse of(final ChatRoom chatRoom, final Game game) {
+        return ChatRoomDetailResponse.builder()
+                .id(chatRoom.getId())
+                .roomName(chatRoom.getName())
+                .type(chatRoom.getType())
+                .domainId(game.getId())
+                .memberCount(chatRoom.getMemberCount())
+                .maxMemberCount(chatRoom.getMaxMemberCount())
+                .playStartTime(game.getPlayStartTime())
+                .playTimeMinutes(game.getPlayTimeMinutes())
+                .members(getChatMemberResponses(chatRoom))
+                .createdAt(chatRoom.getCreatedAt())
+                .build();
+    }
+
+    public static ChatRoomDetailResponse of(final ChatRoom chatRoom, final Crew crew) {
+        return ChatRoomDetailResponse.builder()
+                .id(chatRoom.getId())
+                .roomName(chatRoom.getName())
+                .roomIconImageUrl(crew.getProfileImageUrl())
+                .type(chatRoom.getType())
+                .domainId(crew.getId())
+                .memberCount(chatRoom.getMemberCount())
+                .maxMemberCount(chatRoom.getMaxMemberCount())
+                .members(getChatMemberResponses(chatRoom))
+                .createdAt(chatRoom.getCreatedAt())
+                .build();
+    }
+
+    private static List<ChatMemberResponse> getChatMemberResponses(final ChatRoom chatRoom) {
+        return chatRoom.getMembersInRoom()
+                .stream()
+                .map(ChatMemberResponse::from)
+                .toList();
+    }
+}
