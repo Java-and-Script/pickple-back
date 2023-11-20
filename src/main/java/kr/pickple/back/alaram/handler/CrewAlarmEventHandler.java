e9dd6cc33642a66464693f9a5cab5a754a16c4b0
jay-so <merry9504@naver.com>
2023-11-21 04:46:42 +0900

    refactor: CrewAlarmEventHander,GameAlarmEventHandler 리팩토링

--- src/main/java/kr/pickple/back/alaram/handler/CrewAlarmEventHandler.java
+++ src/main/java/kr/pickple/back/alaram/handler/CrewAlarmEventHandler.java
@@ -1,6 +1,6 @@
 package kr.pickple.back.alaram.handler;
 
-import kr.pickple.back.alaram.domain.CrewAlarm;
+import kr.pickple.back.alaram.dto.response.CrewAlarmResponse;
 import kr.pickple.back.alaram.event.crew.CrewJoinRequestNotificationEvent;
 import kr.pickple.back.alaram.event.crew.CrewMemberJoinedEvent;
 import kr.pickple.back.alaram.event.crew.CrewMemberRejectedEvent;
@@ -19,21 +19,21 @@ public class CrewAlarmEventHandler {
     @Async
     @EventListener
     public void sendAlarmToCrewLeader(final CrewJoinRequestNotificationEvent crewJoinRequestNotificationEvent) {
-        final CrewAlarm crewAlarm = crewAlarmService.createCrewJoinAlaram(crewJoinRequestNotificationEvent); // 알람 생성
+        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewJoinAlaram(crewJoinRequestNotificationEvent); // 알람 생성
         crewAlarmService.emitMessage(crewAlarm); // SSE로 알람 메시지 전송
     }
 
     @Async
     @EventListener
     public void sendAlarmToCrewMemberOnJoin(final CrewMemberJoinedEvent crewMemberJoinedEvent) {
-        final CrewAlarm crewAlarm = crewAlarmService.createCrewMemberApproveAlaram(crewMemberJoinedEvent); // 알람 생성
+        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewMemberApproveAlarm(crewMemberJoinedEvent); // 알람 생성
         crewAlarmService.emitMessage(crewAlarm); // SSE로 알람 메시지 전송
     }
 
     @Async
     @EventListener
     public void sendAlarmToCrewMemberOnRejection(final CrewMemberRejectedEvent crewMemberRejectedEvent) {
-        final CrewAlarm crewAlarm = crewAlarmService.createCrewMemberDeniedAlaram(crewMemberRejectedEvent); // 알람 생성
+        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewMemberDeniedAlarm(crewMemberRejectedEvent); // 알람 생성
         crewAlarmService.emitMessage(crewAlarm); // SSE로 알람 메시지 전송
     }
 }
--- src/main/java/kr/pickple/back/alaram/handler/GameAlarmEventHandler.java
+++ src/main/java/kr/pickple/back/alaram/handler/GameAlarmEventHandler.java
@@ -1,6 +1,6 @@
 package kr.pickple.back.alaram.handler;
 
-import kr.pickple.back.alaram.domain.GameAlarm;
+import kr.pickple.back.alaram.dto.response.GameAlarmResponse;
 import kr.pickple.back.alaram.event.game.GameJoinRequestNotificationEvent;
 import kr.pickple.back.alaram.event.game.GameMemberJoinedEvent;
 import kr.pickple.back.alaram.event.game.GameMemberRejectedEvent;
@@ -19,21 +19,21 @@ public class GameAlarmEventHandler {
     @Async
     @EventListener
     public void sendAlarmToGameHost(final GameJoinRequestNotificationEvent gameJoinRequestNotificationEvent) {
-        final GameAlarm gameAlarm = gameAlarmService.createGameJoinAlaram(gameJoinRequestNotificationEvent); //알람 생성
+        final GameAlarmResponse gameAlarm = gameAlarmService.createGameJoinAlarm(gameJoinRequestNotificationEvent); //알람 생성
         gameAlarmService.emitMessage(gameAlarm); //SSE로 알람 메시지 전송 - sseServcie로 변경?
     }
 
     @Async
     @EventListener
     public void sendAlarmToGameMemberOnJoin(final GameMemberJoinedEvent gameMemberJoinedEvent) {
-        final GameAlarm gameAlarm = gameAlarmService.createGuestApproveAlaram(gameMemberJoinedEvent); //알람 생성
+        final GameAlarmResponse gameAlarm = gameAlarmService.createGuestApproveAlarm(gameMemberJoinedEvent); //알람 생성
         gameAlarmService.emitMessage(gameAlarm); //SSE로 알람 메시지 전송
     }
 
     @Async
     @EventListener
     public void sendAlarmToGameMemberOnRejection(final GameMemberRejectedEvent gameMemberRejectedEvent) {
-        final GameAlarm gameAlarm = gameAlarmService.createGuestDeniedAlarm(gameMemberRejectedEvent); //알람 생성
+        final GameAlarmResponse gameAlarm = gameAlarmService.createGuestDeniedAlarm(gameMemberRejectedEvent); //알람 생성
         gameAlarmService.emitMessage(gameAlarm); //SSE로 알람 메시지 전송
     }
 }
