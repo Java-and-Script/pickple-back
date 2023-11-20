24dbf2f9df46ad33777e252247cf003e6f23b5dc
kylekim2123 <kyle2123@naver.com>
2023-11-20 16:30:06 +0900

    feat: 채팅 관련 웹소켓 환경설정 추가

---
+++ src/main/java/kr/pickple/back/chat/config/ChatConfig.java
@@ -0,0 +1,25 @@
+package kr.pickple.back.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/receive");
+        registry.setApplicationDestinationPrefixes("/send");
+    }
+}
