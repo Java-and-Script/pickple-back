24479ac2370c47a9eaacd7bc9ad8ad362ba28f5a
jay-so <merry9504@naver.com>
2023-11-21 00:16:04 +0900

    feat: Crew,Game 알람 Repository 읽음 상태 변화 메소드 구현

--- src/main/java/kr/pickple/back/alaram/repository/CrewAlarmRepository.java
+++ src/main/java/kr/pickple/back/alaram/repository/CrewAlarmRepository.java
@@ -1,7 +1,9 @@
 package kr.pickple.back.alaram.repository;
 
+import kr.pickple.back.alaram.domain.AlarmStatus;
 import kr.pickple.back.alaram.domain.CrewAlarm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CrewAlarmRepository extends JpaRepository<CrewAlarm, Long> {
+    boolean existsByMemberIdAndIsRead(final Long memberId, final AlarmStatus alarmStatus);
 }
