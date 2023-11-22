e95316c1d47ab8ff8a0fba9a79f828d7d510250a
jay-so <merry9504@naver.com>
2023-11-22 21:19:06 +0900

    refactor: 이벤트 발생 시 해당 크루, 해당 게임을 통해 호스트 정보를 가져오도록 리팩토링

--- src/main/java/kr/pickple/back/alarm/event/crew/CrewJoinRequestNotificationEvent.java
+++ src/main/java/kr/pickple/back/alarm/event/crew/CrewJoinRequestNotificationEvent.java
@@ -1,13 +1,19 @@
 package kr.pickple.back.alarm.event.crew;
 
+import kr.pickple.back.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class CrewJoinRequestNotificationEvent {
 
     private final Long crewId;
+    private final Member crewLeader;
 
-    public Long getCrewId(){
+    public Long getCrewId() {
         return crewId;
+    }
+
+    public Member getCrewLeader() {
+        return crewLeader;
     }
 }
