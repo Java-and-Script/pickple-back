fd113f05d95bc741620d2fdcd1c8629020ab088a
jay-so <merry9504@naver.com>
2023-11-22 21:19:06 +0900

    refactor: 이벤트 발생 시 해당 크루, 해당 게임을 통해 호스트 정보를 가져오도록 리팩토링

--- src/main/java/kr/pickple/back/alarm/event/game/GameJoinRequestNotificationEvent.java
+++ src/main/java/kr/pickple/back/alarm/event/game/GameJoinRequestNotificationEvent.java
@@ -1,13 +1,19 @@
 package kr.pickple.back.alarm.event.game;
 
+import kr.pickple.back.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class GameJoinRequestNotificationEvent {
 
     private final Long gameId;
+    private final Member host;
 
     public Long getGameId() {
         return gameId;
+    }
+
+    public Member getHost() {
+        return host;
     }
 }
