2f0eebbe00cde319922a5529a3584dece2466809
changhyeon <changhyeon.h@kakao.com>
2023-11-23 00:18:18 +0900

    feat: redis 연결 설정

---
+++ src/main/java/kr/pickple/back/common/config/CommonConfig.java
@@ -0,0 +1,13 @@
+package kr.pickple.back.common.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import kr.pickple.back.common.config.property.RedisProperties;
+import kr.pickple.back.common.config.property.S3Properties;
+
+@Configuration
+@EnableConfigurationProperties(value = {RedisProperties.class, S3Properties.class})
+public class CommonConfig {
+
+}
