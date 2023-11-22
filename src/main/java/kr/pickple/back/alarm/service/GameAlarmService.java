704f6d613b80304fa90e76f5c4deaa4080e530f6
jay-so <merry9504@naver.com>
2023-11-23 00:24:25 +0900

    refactor: 게임 관련 알람 생성, 수정, 삭제 구현 & 실시간 게임 관련 알람 응답 메시지 리팩토링

--- src/main/java/kr/pickple/back/alarm/service/GameAlarmService.java
+++ src/main/java/kr/pickple/back/alarm/service/GameAlarmService.java
@@ -1,7 +1,7 @@
 package kr.pickple.back.alarm.service;
 
-import kr.pickple.back.alarm.domain.AlarmStatus;
 import kr.pickple.back.alarm.domain.GameAlarm;
+import kr.pickple.back.alarm.dto.request.GameAlarmUpdateStatusRequest;
 import kr.pickple.back.alarm.dto.response.GameAlarmResponse;
 import kr.pickple.back.alarm.event.game.GameJoinRequestNotificationEvent;
 import kr.pickple.back.alarm.event.game.GameMemberJoinedEvent;
@@ -18,24 +18,21 @@ import kr.pickple.back.member.exception.MemberException;
 import kr.pickple.back.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static kr.pickple.back.alarm.domain.AlarmStatus.FALSE;
-import static kr.pickple.back.alarm.domain.AlarmType.*;
+import static kr.pickple.back.alarm.domain.GameAlarmType.*;
 import static kr.pickple.back.alarm.exception.AlarmExceptionCode.ALARM_NOT_FOUND;
 import static kr.pickple.back.common.domain.RegistrationStatus.WAITING;
 import static kr.pickple.back.game.exception.GameExceptionCode.GAME_IS_NOT_HOST;
 import static kr.pickple.back.game.exception.GameExceptionCode.GAME_NOT_FOUND;
 import static kr.pickple.back.member.exception.MemberExceptionCode.MEMBER_NOT_FOUND;
 
-//SSE 연결 로직에서는 @Transcational금지
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -44,87 +41,73 @@ public class GameAlarmService {
     private final MemberRepository memberRepository;
     private final GameRepository gameRepository;
     private final GameAlarmRepository gameAlarmRepository;
-    //private final AlarmService alarmService;
     private final SseEmitters sseEmitters;
 
     @Transactional
     public GameAlarmResponse createGameJoinAlarm(final GameJoinRequestNotificationEvent gameJoinRequestNotificationEvent) {
-        //1.게임 리포지토리에서 해당 게임의 호스트인지 확인
+
         validateIsHost(gameJoinRequestNotificationEvent);
 
-        //2. 이벤트로부터 게임 정보 가져오기
         final Long gameId = gameJoinRequestNotificationEvent.getGameId();
         final Game game = getGameInfo(gameId);
         final Member host = game.getHost();
 
-        //3. 알람 생성
         final GameAlarm gameAlarm = GameAlarm.builder()
                 .game(game)
                 .member(host)
-                .alarmType(HOST_WAITING)
+                .gameAlarmType(HOST_WAITING)
                 .build();
 
-        //4. DB에다가 알림 저장
         gameAlarmRepository.save(gameAlarm);
 
+
         final GameAlarmResponse response = GameAlarmResponse.of(gameAlarm);
-        //alarmService.notify(host.getId(), response);
+
         sseEmitters.notify(host.getId(), response);
-
-
         return response;
     }
 
     @Transactional
     public GameAlarmResponse createGuestApproveAlarm(final GameMemberJoinedEvent gameMemberJoinedEvent) {
 
-        //1.이벤트로부터 게임 정보, 회원 정보 가져오기
         final Long gameId = gameMemberJoinedEvent.getGameId();
         final Game game = getGameInfo(gameId);
         final Long memberId = gameMemberJoinedEvent.getMemberId();
         final Member member = getMemberInfo(memberId);
 
-        //2.알람 생성
         final GameAlarm gameAlarm = GameAlarm.builder()
                 .game(game)
                 .member(member)
-                .alarmType(GUEST_ACCEPT)
+                .gameAlarmType(GUEST_ACCEPT)
                 .build();
 
-        //3.알람을 DB에다가 저장
         gameAlarmRepository.save(gameAlarm);
 
         final GameAlarmResponse response = GameAlarmResponse.of(gameAlarm);
-//        alarmService.notify(member.getId(), response);
+
         sseEmitters.notify(member.getId(), response);
-
-
         return response;
     }
 
     @Transactional
     public GameAlarmResponse createGuestDeniedAlarm(final GameMemberRejectedEvent gameMemberRejectedEvent) {
 
-        //1.이벤트로부터 게임 정보, 회원 정보 가져오기
         final Long gameId = gameMemberRejectedEvent.getGameId();
         final Game game = getGameInfo(gameId);
         final Long memberId = gameMemberRejectedEvent.getMemberId();
         final Member member = getMemberInfo(memberId);
 
-        //2.알람 생성
         final GameAlarm gameAlarm = GameAlarm.builder()
                 .game(game)
                 .member(member)
-                .alarmType(GUEST_DENIED)
+                .gameAlarmType(GUEST_DENIED)
                 .build();
 
-        //3.알람 DB에다가 저장
         gameAlarmRepository.save(gameAlarm);
 
         final GameAlarmResponse response = GameAlarmResponse.of(gameAlarm);
-//        alarmService.notify(member.getId(), response);
-        sseEmitters.notify(member.getId(), response);
 
+        sseEmitters.notify(member.getId(), response);
         return response;
     }
 
@@ -133,8 +116,7 @@ public class GameAlarmService {
         final Long gameId = gameJoinRequestNotificationEvent.getGameId();
         final Game game = gameRepository.findById(gameId).orElseThrow(() -> new GameException(GAME_NOT_FOUND, gameId));
 
-        //해당 게임의 id와 호스트 id를 비교하여, 해당 게임의 호스트가 맞는지 체크
-        if (!game.isHost(gameJoinRequestNotificationEvent.getGameId())) {
+        if (!game.isHost(gameJoinRequestNotificationEvent.getHost().getId())) {
             throw new GameException(GAME_IS_NOT_HOST, gameId, game.getHost());
         }
     }
@@ -144,7 +126,6 @@ public class GameAlarmService {
                 .orElseThrow(() -> new GameException(GAME_NOT_FOUND, gameId));
         return game;
     }
-
 
     private Member getMemberInfo(final Long memberId) {
         return memberRepository.findById(memberId)
@@ -161,13 +142,11 @@ public class GameAlarmService {
         return game.getMembersByStatus(status);
     }
 
-    //SSE 알람을 발송하는 부분
     public void emitMessage(final GameAlarmResponse gameAlarm) {
         final Long gameId = gameAlarm.getGameId();
         final Member gameHost = getHostOfGame(gameId);
         final List<Member> gameApplyMembers = getGameMembers(gameId, WAITING);
 
-        //1. SSE로 알람 생성 - 각 케이스 별 알람 생성(호스트와 게스트에게 메시지 전송)
         sendAlarmToGameHost(gameHost, gameAlarm);
         sendAlarmToGameApplyMembers(gameApplyMembers, gameAlarm);
     }
@@ -184,91 +163,43 @@ public class GameAlarmService {
         }
     }
 
-    //게임 호스트에게 가입 신청이 올 시 받는 알람
     private void sendAlarmToGameHost(final Member gameHost, final GameAlarmResponse gameAlarm) {
         sendAlarmToMember(gameHost, gameAlarm);
     }
 
-    //회원(게스트 - 대기)에게 호스트가 승락 시, 상태가 Confirmed로 변하며 승락되었다는 알람
-    //회원(게스트 - 대기)에게 호스트가 거절 시, 게임 참여자(Game Memmber) 테이블에서 삭제되며, 거절되었다는 알람
     private void sendAlarmToGameApplyMembers(List<Member> members, GameAlarmResponse gameAlarm) {
         for (final Member member : members) {
             sendAlarmToMember(member, gameAlarm);
         }
     }
 
-    //저장된 게임  알람을 모두 찾기 위한 메소드
-    public List<GameAlarmResponse> findGameAlarmAll() {
-        //1.DB에서 모든 GameAlarm을 조회
-        List<GameAlarm> gameAlarms = gameAlarmRepository.findAll();
-
-        //2.조회한 GameAlarm을 GameAlarmResponse로 변환
-        List<GameAlarmResponse> gameAlarmResponses = gameAlarms.stream()
-                .map(GameAlarmResponse::of)
-                .collect(Collectors.toList());
-
-        //3.변환한 GameAlarmResponse를 반환
-        return gameAlarmResponses;
-    }
-
-    //게임 알람에서 isRead가 False가 있는지 체크하는 메소드
     public boolean checkUnreadGameAlarm(final Long memberId) {
-        //1. 해당 회원의 읽지 않은 알람이 있는지 체크함
         final boolean existsUnreadGameAlarm = gameAlarmRepository.existsByMemberIdAndIsRead(memberId, FALSE);
-
-        //2. 반환
         return existsUnreadGameAlarm;
     }
 
-    //게임 알림 찾기 By ID
-    public GameAlarm findGameAlarmById(final Long gameAlarmId) {
-        //1.알람 ID로 해당 알림 찾기
-        final GameAlarm gameAlarm = checkExistGameAlarm(gameAlarmId);
-
-        //2.찾은 알람 반환
-        return gameAlarm;
+    public void deleteAllGameAlarms(final Long memberId) {
+        gameAlarmRepository.deleteByMemberId(memberId);
     }
 
-    //게임 알림 변경 By ID
-    public void updateGameAlarmStatus(final Long gameAlarmId, final String isRead) {
-        //1. 알람 ID로 해당 알림 찾기
-        final GameAlarm gameAlarm = checkExistGameAlarm(gameAlarmId);
-        final AlarmStatus alarmStatus = AlarmStatus.from(isRead);
+    public void updateGameAlarmById(
+            final Long loggedInMemberId,
+            final Long gameAlarmId,
+            final GameAlarmUpdateStatusRequest gameAlarmUpdateStatusRequest
+    ) {
+        final Member member = findMemberById(loggedInMemberId);
+        final GameAlarm gameAlarm = checkExistGameAlarm(loggedInMemberId, gameAlarmId);
 
-        //2. 상태 업데이트
-        gameAlarm.updateStatus(alarmStatus);
-
-        //3. 저장
-        gameAlarmRepository.save(gameAlarm);
+        gameAlarm.updateStatus(gameAlarmUpdateStatusRequest.getIsRead());
     }
 
-    private GameAlarm checkExistGameAlarm(final Long gameAlarmId) {
-        final GameAlarm gameAlarm = gameAlarmRepository.findById(gameAlarmId)
-                .orElseThrow(() -> new AlarmException(ALARM_NOT_FOUND, gameAlarmId));
-
-        return gameAlarm;
+    private Member findMemberById(final Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(MEMBER_NOT_FOUND, memberId));
     }
 
-    //게임의 모든 알림을 삭제
-    public void deleteAllGameAlarms() {
-        //1.DB에서 생성된 모든 게임 알람을 삭제함
-        gameAlarmRepository.deleteAll();
-    }
-
-    //1. memberId와 cursorid를 이용하여 GameAlarm이 있는지 확인하고 반환
-    public boolean existsGameAlarmByMemberIdAndIdLessThan(final Long memberId, final Long id) {
-        return gameAlarmRepository.existsByMemberIdAndIdLessThan(memberId, id);
-    }
-
-    //2.memberId와 cursorid를 이용하여 GameAlarm을 가져오며, 그 결과를 GameAlarmResponse로 변환하여 반환
-    public List<GameAlarmResponse> findGameAlarmByMemberIdOrderByIdDesc(final Long memberId, final Pageable page) {
-        List<GameAlarm> gameAlarms = gameAlarmRepository.findByMemberIdOrderByIdDesc(memberId, page);
-        return gameAlarms.stream().map(GameAlarmResponse::of).collect(Collectors.toList());
-    }
-
-    //3.memberId와 id를 이용하여 GameAlarm을 가져오고, 그 결과를 GameAlarmResponse로 변환하여 반환
-    public List<GameAlarmResponse> findGameAlarmByMemberIdAndIdLessThanOrderByIdDesc(final Long memberId, final Long cursorId, final Pageable page) {
-        List<GameAlarm> gameAlarms = gameAlarmRepository.findByMemberIdAndIdLessThanOrderByIdDesc(memberId, cursorId, page);
-        return gameAlarms.stream().map(GameAlarmResponse::of).collect(Collectors.toList());
+    private GameAlarm checkExistGameAlarm(final Long memberId, final Long gameAlarmId) {
+        return gameAlarmRepository.findByMemberIdAndId(memberId, gameAlarmId)
+                .orElseThrow(() -> new AlarmException(ALARM_NOT_FOUND, memberId, gameAlarmId));
     }
 }
