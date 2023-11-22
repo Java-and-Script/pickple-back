c28b5c1e4a62a4c332b9b5b36ba501ef88c04e74
jay-so <merry9504@naver.com>
2023-11-23 00:40:43 +0900

    refactor: 크루 알람, 게임 알람 리포지토리 리팩토링

--- src/main/java/kr/pickple/back/alarm/repository/AlarmRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.pickple.back.alarm.repository;
-
-import kr.pickple.back.alarm.domain.Alarm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlarmRepository extends JpaRepository<Alarm, Long> {
-    Boolean existsByIdLessThan(final Long id);
-}
--- src/main/java/kr/pickple/back/alarm/repository/GameAlarmRepository.java
+++ src/main/java/kr/pickple/back/alarm/repository/GameAlarmRepository.java
@@ -2,18 +2,15 @@ package kr.pickple.back.alarm.repository;
 
 import kr.pickple.back.alarm.domain.AlarmStatus;
 import kr.pickple.back.alarm.domain.GameAlarm;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface GameAlarmRepository extends JpaRepository<GameAlarm, Long> {
 
-    List<GameAlarm> findByMemberIdAndIdLessThanOrderByIdDesc(final Long memberId, final Long id, final Pageable page);
-
-    boolean existsByMemberIdAndIdLessThan(final Long memberId, final Long id);
-
     boolean existsByMemberIdAndIsRead(final Long memberId, final AlarmStatus alarmStatus);
 
-    List<GameAlarm> findByMemberIdOrderByIdDesc(final Long memberId, final Pageable page);
+    void deleteByMemberId(final Long memberId);
+
+    Optional<GameAlarm> findByMemberIdAndId(final Long memberId, final Long gameAlarmId);
 }
