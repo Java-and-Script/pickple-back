1faaf3524b30cb18359b0ffa4c67d47e05a7e13e
jay-so <merry9504@naver.com>
2023-11-22 21:21:43 +0900

    style: 불필요한 주석 제거

--- src/main/java/kr/pickple/back/alarm/handler/CrewAlarmEventHandler.java
+++ src/main/java/kr/pickple/back/alarm/handler/CrewAlarmEventHandler.java
@@ -21,23 +21,23 @@ public class CrewAlarmEventHandler {
     @Transactional
     @EventListener
     public void sendAlarmToCrewLeader(final CrewJoinRequestNotificationEvent crewJoinRequestNotificationEvent) {
-        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewJoinAlarm(crewJoinRequestNotificationEvent); // 알람 생성
-        crewAlarmService.emitMessage(crewAlarm); // SSE로 알람 메시지 전송
+        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewJoinAlarm(crewJoinRequestNotificationEvent);
+        crewAlarmService.emitMessage(crewAlarm);
     }
 
     @Async
     @Transactional
     @EventListener
     public void sendAlarmToCrewMemberOnJoin(final CrewMemberJoinedEvent crewMemberJoinedEvent) {
-        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewMemberApproveAlarm(crewMemberJoinedEvent); // 알람 생성
-        crewAlarmService.emitMessage(crewAlarm); // SSE로 알람 메시지 전송
+        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewMemberApproveAlarm(crewMemberJoinedEvent);
+        crewAlarmService.emitMessage(crewAlarm);
     }
 
     @Async
     @Transactional
     @EventListener
     public void sendAlarmToCrewMemberOnRejection(final CrewMemberRejectedEvent crewMemberRejectedEvent) {
-        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewMemberDeniedAlarm(crewMemberRejectedEvent); // 알람 생성
-        crewAlarmService.emitMessage(crewAlarm); // SSE로 알람 메시지 전송
+        final CrewAlarmResponse crewAlarm = crewAlarmService.createCrewMemberDeniedAlarm(crewMemberRejectedEvent);
+        crewAlarmService.emitMessage(crewAlarm);
     }
 }
