8025539c305966341001daf45ad88982ba60475c
changhyeon <changhyeon.h@kakao.com>
2023-11-23 00:18:59 +0900

    feat: RefreshToken Redis에 저장하는 기능 구현

--- src/main/java/kr/pickple/back/auth/domain/token/RefreshToken.java
+++ src/main/java/kr/pickple/back/auth/domain/token/RefreshToken.java
@@ -1,24 +1,20 @@
 package kr.pickple.back.auth.domain.token;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
+@Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken {
 
-    @Id
     private String token;
-
-    @Getter
-    @NotNull
     private Long memberId;
+    private LocalDateTime createdAt;
 }
