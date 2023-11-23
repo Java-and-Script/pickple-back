9aa923ad1f9c0fddd7dca8ee1e9e726aeba20a63
jay-so <merry9504@naver.com>
2023-11-24 02:40:00 +0900

    style: 에러 코드, 불필요한 개행 수정

--- src/main/java/kr/pickple/back/alarm/exception/AlarmExceptionCode.java
+++ src/main/java/kr/pickple/back/alarm/exception/AlarmExceptionCode.java
@@ -9,12 +9,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AlarmExceptionCode implements ExceptionCode {
 
-    ALARM_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-001", "알람 읽음 여부를 찾을 수 없음"),
-    ALARM_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-002", "알림 타입을 찾을 수 없음"),
-    ALARM_EXISTS_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-003", "사용자의 알람 상태 여부를 찾을 수 없음"),
-    ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "AEC-004", "해당 알람을 찾을 수 없음"),
+    ALARM_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-001", "알람 읽음 여부를 찾을 수 없음"),
+    ALARM_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-002", "알림 타입을 찾을 수 없음"),
+    ALARM_EXISTS_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-003", "사용자의 알람 상태 여부를 찾을 수 없음"),
+    ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-004", "해당 알람을 찾을 수 없음"),
     ;
-
 
     private final HttpStatus status;
     private final String code;
