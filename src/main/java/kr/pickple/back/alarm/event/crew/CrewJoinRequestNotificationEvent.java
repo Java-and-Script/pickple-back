0f301a3c57a361054e79645f61d872a5c4aba728
jay-so <merry9504@naver.com>
2023-11-24 15:51:12 +0900

    refactor: Crew 이벤트 lombok을 사용하여 @Getter,@Builder을 사용하게 리팩토링

--- src/main/java/kr/pickple/back/alarm/event/crew/CrewJoinRequestNotificationEvent.java
+++ src/main/java/kr/pickple/back/alarm/event/crew/CrewJoinRequestNotificationEvent.java
@@ -1,19 +1,15 @@
 package kr.pickple.back.alarm.event.crew;
 
 import kr.pickple.back.member.domain.Member;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
+@Builder
 @RequiredArgsConstructor
 public class CrewJoinRequestNotificationEvent {
 
     private final Long crewId;
-    private final Member crewLeader;
-
-    public Long getCrewId() {
-        return crewId;
-    }
-
-    public Member getCrewLeader() {
-        return crewLeader;
-    }
+    private final Member memberId;
 }
