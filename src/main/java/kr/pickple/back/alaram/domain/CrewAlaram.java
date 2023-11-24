2605c5e278968310039078b3c1aec10a4be8dc7b
jay-so <merry9504@naver.com>
2023-11-25 02:22:39 +0900

    feat: Crew, Game 알람 도메인 구현

---
+++ src/main/java/kr/pickple/back/alaram/domain/CrewAlaram.java
@@ -0,0 +1,61 @@
+package kr.pickple.back.alaram.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import kr.pickple.back.alaram.util.AlaramTypeConverter;
+import kr.pickple.back.common.domain.BaseEntity;
+import kr.pickple.back.crew.domain.Crew;
+import kr.pickple.back.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+import static kr.pickple.back.alaram.domain.AlaramStatus.FALSE;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CrewAlaram extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(length = 10)
+    @Convert(converter = AlaramStatus.class)
+    private AlaramStatus isRead = FALSE;
+
+    @NotNull
+    @Column(length = 20)
+    @Convert(converter = AlaramTypeConverter.class)
+    private AlaramType alaramType;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "crew_id")
+    private Crew crew;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    private CrewAlaram(
+            final AlaramType alaramType,
+            final Crew crew,
+            final Member member
+    ) {
+        this.alaramType = alaramType;
+        this.createdAt = super.getCreatedAt();
+        this.crew = crew;
+        this.member = member;
+    }
+}
--- src/main/java/kr/pickple/back/common/domain/BaseEntity.java
+++ src/main/java/kr/pickple/back/common/domain/BaseEntity.java
@@ -1,16 +1,14 @@
 package kr.pickple.back.common.domain;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.ColumnDefault;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
