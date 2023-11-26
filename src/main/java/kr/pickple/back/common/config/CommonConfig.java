b0bdc91ceacb0228956e0f8b6fa28f2c0b7eb961
jay-so <merry9504@naver.com>
2023-11-26 16:54:33 +0900

    fix: 크루 거절 알람, 게임 거절 알람 트랜잭션 문제로 인한 크루 알람, 게임 알람 이벤트 핸들러 리팩토링

--- src/main/java/kr/pickple/back/common/config/CommonConfig.java
+++ src/main/java/kr/pickple/back/common/config/CommonConfig.java
@@ -1,10 +1,9 @@
 package kr.pickple.back.common.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
 import kr.pickple.back.common.config.property.RedisProperties;
 import kr.pickple.back.common.config.property.S3Properties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(value = {RedisProperties.class, S3Properties.class})
