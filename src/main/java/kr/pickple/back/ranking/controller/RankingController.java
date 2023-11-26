484cd72320d09de9685a8e7c133afff8298574e9
charlesuu <tiger0192004@gmail.com>
2023-11-26 23:20:37 +0900

    feat: 랭킹 컨트롤러 구현

---
+++ src/main/java/kr/pickple/back/ranking/controller/RankingController.java
@@ -0,0 +1,27 @@
+package kr.pickple.back.ranking.controller;
+
+import static org.springframework.http.HttpStatus.*;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.pickple.back.ranking.dto.CrewRankingResponse;
+import kr.pickple.back.ranking.service.RankingService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/ranking")
+public class RankingController {
+
+    private final RankingService rankingService;
+
+    @GetMapping("/crews")
+    public ResponseEntity<List<CrewRankingResponse>> findCrewRanking() {
+        return ResponseEntity.status(OK).body(rankingService.findCrewRanking());
+    }
+}
