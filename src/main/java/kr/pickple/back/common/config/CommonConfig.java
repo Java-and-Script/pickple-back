765ab4baad2f57bbc58558a0eda81b35d6aee160
charlesuu <tiger0192004@gmail.com>
2023-11-30 02:27:17 +0900

    feat: 랭킹 갱신을 위한 스케줄러 추가

--- src/main/java/kr/pickple/back/common/config/CommonConfig.java
+++ src/main/java/kr/pickple/back/common/config/CommonConfig.java
@@ -1,12 +1,15 @@
 package kr.pickple.back.common.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import kr.pickple.back.common.config.property.AsyncProperties;
 import kr.pickple.back.common.config.property.RedisProperties;
 import kr.pickple.back.common.config.property.S3Properties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableScheduling
 @EnableConfigurationProperties(value = {RedisProperties.class, S3Properties.class, AsyncProperties.class})
 public class CommonConfig {
 
