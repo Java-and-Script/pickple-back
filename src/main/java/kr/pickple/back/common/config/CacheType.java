1ed85e576674209df935a00b373e0694be7ea988
charlesuu <tiger0192004@gmail.com>
2023-11-30 02:25:15 +0900

    feat: 캐시 타입 추가

--- src/main/java/kr/pickple/back/common/config/CacheType.java
+++ src/main/java/kr/pickple/back/common/config/CacheType.java
@@ -9,9 +9,15 @@ public enum CacheType {
 
     POLYGON("polygon", Integer.MAX_VALUE, 1000),
     ADDRESS("address", Integer.MAX_VALUE, 1000),
+    RANKING("ranking", Constants.SIX_HOURS_IN_SECONDS, 10),
     ;
 
     private final String cacheName;
     private final int expireAfterWrite;
     private final int maximumSize;
+
+    private static class Constants {
+
+        public static final int SIX_HOURS_IN_SECONDS = 60 * 60 * 6;
+    }
 }
