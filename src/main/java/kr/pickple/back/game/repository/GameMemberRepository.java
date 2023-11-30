afc3bef9bf5fc35e3b1bea7cb34d17abeca87d1a
kylekim2123 <kyle2123@naver.com>
2023-11-30 17:23:14 +0900

    fix: 경기 참여가 확정된 사용자만 매너 스코어 리뷰 할 수 있도록 예외처리 추가

--- src/main/java/kr/pickple/back/game/repository/GameMemberRepository.java
+++ src/main/java/kr/pickple/back/game/repository/GameMemberRepository.java
@@ -4,9 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import kr.pickple.back.common.domain.RegistrationStatus;
 import kr.pickple.back.game.domain.GameMember;
 
 public interface GameMemberRepository extends JpaRepository<GameMember, Long> {
 
     Optional<GameMember> findByMemberIdAndGameId(final Long memberId, final Long gameId);
+
+    Optional<GameMember> findByMemberIdAndGameIdAndStatus(
+            final Long memberId,
+            final Long gameId,
+            final RegistrationStatus status
+    );
 }
