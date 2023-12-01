674bb773aa941bea9b2eff758917c34ca8518eb0
jay-so <merry9504@naver.com>
2023-12-01 20:01:49 +0900

    feat: RedisEventCacheRepository 구현

---
+++ src/main/java/kr/pickple/back/alarm/repository/RedisEventCacheRepository.java
@@ -0,0 +1,34 @@
+package kr.pickple.back.alarm.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.Set;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisEventCacheRepository {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void saveEventCache(final String eventCacheId, final Object event) {
+        redisTemplate.opsForHash().put("EventCache", eventCacheId, event);
+    }
+
+    public Map<Object, Object> findAllEventCacheByMemberId(final Long memberId) {
+        return redisTemplate.opsForHash().entries("EventCache:" + memberId);
+    }
+
+    public void deleteEventCache(final String eventCacheId) {
+        redisTemplate.opsForHash().delete("EventCache", eventCacheId);
+    }
+
+    public void deleteAllEventCacheStartWithId(final Long memberId) {
+        Set<String> keys = redisTemplate.keys("EventCache:" + String.valueOf(memberId) + "*");
+        if (keys != null && !keys.isEmpty()) {
+            redisTemplate.delete(keys);
+        }
+    }
+}
