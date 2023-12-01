ae8c56a03efec1d1331162af5ec8a3cedf42e623
changhyeon <changhyeon.h@kakao.com>
2023-12-01 16:21:34 +0900

    feat: Domain과 Repository에 조회, 업데이트 관련 설정

--- src/main/java/kr/pickple/back/game/repository/GameRepository.java
+++ src/main/java/kr/pickple/back/game/repository/GameRepository.java
@@ -1,15 +1,19 @@
 package kr.pickple.back.game.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kr.pickple.back.address.domain.AddressDepth1;
 import kr.pickple.back.address.domain.AddressDepth2;
 import kr.pickple.back.chat.domain.ChatRoom;
 import kr.pickple.back.game.domain.Game;
+import kr.pickple.back.game.domain.GameStatus;
 
 public interface GameRepository extends JpaRepository<Game, Long>, GameSearchRepository {
 
@@ -20,4 +24,10 @@ public interface GameRepository extends JpaRepository<Game, Long>, GameSearchRep
     );
 
     Optional<Game> findByChatRoom(final ChatRoom chatRoom);
+
+    @Query("SELECT g FROM Game g WHERE g.status = :status AND CONCAT(g.playDate, ' ', g.playStartTime) <= DATE_FORMAT(:nowDateTime, '%Y-%m-%d %H:%i:%s')")
+    List<Game> findGamesByStatusAndPlayDateStartTimeBeforeNow(final GameStatus status, final LocalDateTime nowDateTime);
+
+    @Query("SELECT g FROM Game g WHERE g.status = :status AND CONCAT(g.playDate, ' ', g.playEndTime) <= DATE_FORMAT(:nowDateTime, '%Y-%m-%d %H:%i:%s')")
+    List<Game> findGamesByStatusAndPlayDateEndTimeBeforeNow(final GameStatus status, final LocalDateTime nowDateTime);
 }
