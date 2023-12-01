9c68a717a1d4f8d81d984a5626bc75c107fc1a34
changhyeon <changhyeon.h@kakao.com>
2023-12-01 16:21:34 +0900

    feat: Domain과 Repository에 조회, 업데이트 관련 설정

--- src/main/java/kr/pickple/back/game/domain/Game.java
+++ src/main/java/kr/pickple/back/game/domain/Game.java
@@ -147,6 +147,10 @@ public class Game extends BaseEntity {
         updateGamePositions(positions);
     }
 
+    public void updateGameStatus(final GameStatus gameStatus) {
+        this.status = gameStatus;
+    }
+
     private void updateGamePositions(final List<Position> positions) {
         gamePositions.updateGamePositions(this, positions);
     }
