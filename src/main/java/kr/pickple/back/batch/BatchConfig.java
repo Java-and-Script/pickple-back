58e000b1a3683afc648a990cf863ec4f82fd1909
changhyeon <changhyeon.h@kakao.com>
2023-12-01 17:10:43 +0900

    refactor: Tasklet을 Bean으로 등록하여 의존성 주입 방법으로 변경

--- src/main/java/kr/pickple/back/batch/BatchConfig.java
+++ src/main/java/kr/pickple/back/batch/BatchConfig.java
@@ -6,19 +6,15 @@ import org.springframework.batch.core.configuration.support.DefaultBatchConfigur
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import kr.pickple.back.game.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig extends DefaultBatchConfiguration {
-
-    private final GameRepository gameRepository;
 
     @Bean
     public Job job(
@@ -33,19 +29,9 @@ public class BatchConfig extends DefaultBatchConfiguration {
     }
 
     @Bean
-    public Tasklet gameClosedTasklet() {
-        return new GameClosedTasklet(gameRepository);
-    }
-
-    @Bean
-    public Tasklet gameEndedTasklet() {
-        return new GameEndedTasklet(gameRepository);
-    }
-
-    @Bean
     public Step updateGameStatusToClosedStep(
             final JobRepository jobRepository,
-            final Tasklet gameClosedTasklet,
+            final GameClosedTasklet gameClosedTasklet,
             final PlatformTransactionManager transactionManager
     ) {
         return new StepBuilder("updateGameStatusToClosedStep", jobRepository)
@@ -56,7 +42,7 @@ public class BatchConfig extends DefaultBatchConfiguration {
     @Bean
     public Step updateGameStatusToEndedStep(
             final JobRepository jobRepository,
-            final Tasklet gameEndedTasklet,
+            final GameEndedTasklet gameEndedTasklet,
             final PlatformTransactionManager transactionManager
     ) {
         return new StepBuilder("updateGameStatusToEndedStep", jobRepository)
--- src/main/java/kr/pickple/back/batch/GameEndedTasklet.java
+++ src/main/java/kr/pickple/back/batch/GameEndedTasklet.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
 import kr.pickple.back.game.domain.Game;
 import kr.pickple.back.game.repository.GameRepository;
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class GameEndedTasklet implements Tasklet {
 
