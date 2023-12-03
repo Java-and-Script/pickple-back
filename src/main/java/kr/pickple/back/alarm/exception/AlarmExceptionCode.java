873a14ba175e80ae9121babdb54a4880f06abcdb
jay-so <merry9504@naver.com>
2023-12-03 17:25:31 +0900

    refactor: 커스텀 알람 에러 메시지 리팩토링

--- src/main/java/kr/pickple/back/alarm/exception/AlarmExceptionCode.java
+++ src/main/java/kr/pickple/back/alarm/exception/AlarmExceptionCode.java
@@ -13,6 +13,7 @@ public enum AlarmExceptionCode implements ExceptionCode {
     ALARM_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-002", "알림 타입을 찾을 수 없음"),
     ALARM_EXISTS_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-003", "사용자의 알람 상태 여부를 찾을 수 없음"),
     ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-004", "해당 알람을 찾을 수 없음"),
+    ALARM_CONVERT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "ALA-005", "알람 타입 변환 중 오류 발생"),
     ;
 
     private final HttpStatus status;
