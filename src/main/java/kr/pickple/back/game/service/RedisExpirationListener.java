3c95ac31ff7fd041964a4bf32c596cc19c490cd6
changhyeon <changhyeon.h@kakao.com>
2024-01-19 18:37:58 +0900

    feat: Redis의 키 만료 이벤트를 스프링에서 컨슈밍하도록 설정

---
+++ src/main/java/kr/pickple/back/game/service/RedisExpirationListener.java
@@ -0,0 +1,32 @@
+package kr.pickple.back.game.service;
+
+import java.util.StringTokenizer;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.stereotype.Component;
+
+import kr.pickple.back.game.domain.GameStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisExpirationListener implements MessageListener {
+
+    private final GameService gameService;
+
+    @Override
+    public void onMessage(final Message message, final byte[] pattern) {
+        final StringTokenizer stringTokenizer = new StringTokenizer(message.toString(), ":");
+        final String keyName = stringTokenizer.nextToken();
+
+        if (keyName.equals("game")) {
+            final GameStatus gameStatus = GameStatus.valueOf(stringTokenizer.nextToken());
+            final Long gameId = Long.parseLong(stringTokenizer.nextToken());
+
+            gameService.updateGameStatus(gameStatus, gameId);
+        }
+    }
+}
