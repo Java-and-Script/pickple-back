3c873455d5fbda478e769112dad2225245127a94
kylekim2123 <kyle2123@naver.com>
2024-02-14 10:36:21 +0900

    refactor: Crew 도메인 구현계층 초기세팅

---
+++ src/main/java/kr/pickple/back/crew/implement/CrewWriter.java
@@ -0,0 +1,11 @@
+package kr.pickple.back.crew.implement;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CrewWriter {
+
+}
