b2810d7c95b79471b868fdba243abd2818c05a2b
kylekim2123 <kyle2123@naver.com>
2024-02-23 23:16:09 +0900

    refactor: Member DTO와 Domain 객체를 상호 변환하는 Mapper 객체 생성

--- src/main/java/kr/pickple/back/member/dto/mapper/MemberRequestMapper.java
+++ src/main/java/kr/pickple/back/member/dto/mapper/MemberRequestMapper.java
@@ -2,7 +2,10 @@ package kr.pickple.back.member.dto.mapper;
 
 import kr.pickple.back.member.domain.NewMember;
 import kr.pickple.back.member.dto.request.MemberCreateRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MemberRequestMapper {
 
     public static NewMember mapToNewMemberDomain(final MemberCreateRequest memberCreateRequest) {
--- src/main/java/kr/pickple/back/member/mapper/MemberMapper.java
+++ src/main/java/kr/pickple/back/member/mapper/MemberMapper.java
@@ -1,16 +1,36 @@
 package kr.pickple.back.member.mapper;
 
+import static kr.pickple.back.common.domain.RegistrationStatus.*;
+
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import kr.pickple.back.address.dto.response.MainAddress;
+import kr.pickple.back.address.implement.AddressReader;
+import kr.pickple.back.crew.implement.CrewReader;
+import kr.pickple.back.game.domain.Game;
+import kr.pickple.back.game.repository.GameMemberRepository;
+import kr.pickple.back.game.repository.GameRepository;
 import kr.pickple.back.member.domain.Member;
+import kr.pickple.back.member.domain.MemberDomain;
 import kr.pickple.back.member.domain.MemberPosition;
 import kr.pickple.back.member.domain.MemberProfile;
 import kr.pickple.back.member.domain.MemberStatus;
 import kr.pickple.back.member.domain.NewMember;
+import kr.pickple.back.member.repository.MemberPositionRepository;
 import kr.pickple.back.position.domain.Position;
+import lombok.RequiredArgsConstructor;
 
-public final class MemberMapper {
+@Component
+@RequiredArgsConstructor
+public class MemberMapper {
+
+    private final AddressReader addressReader;
+    private final CrewReader crewReader;
+    private final MemberPositionRepository memberPositionRepository;
+    private final GameRepository gameRepository;
+    private final GameMemberRepository gameMemberRepository;
 
     public static Member mapToMemberEntity(final NewMember newMember, final MainAddress mainAddress) {
         return Member.builder()
@@ -52,6 +72,39 @@ public final class MemberMapper {
                 .addressDepth1Name(mainAddress.getAddressDepth1().getName())
                 .addressDepth2Name(mainAddress.getAddressDepth2().getName())
                 .positions(positions)
+                .build();
+    }
+
+    public MemberDomain mapToMemberDomain(final Member memberEntity) {
+        final Long memberId = memberEntity.getId();
+        final MainAddress mainAddress = addressReader.readMainAddressById(
+                memberEntity.getAddressDepth1Id(),
+                memberEntity.getAddressDepth2Id()
+        );
+
+        final List<Position> positions = memberPositionRepository.findAllByMemberId(memberId)
+                .stream()
+                .map(MemberPosition::getPosition)
+                .toList();
+
+        final List<Game> joinedGames = gameMemberRepository.findAllByMemberIdAndStatus(memberId, CONFIRMED)
+                .stream()
+                .map(gameMember -> gameRepository.getGameById(gameMember.getGameId()))
+                .toList();
+
+        return MemberDomain.builder()
+                .memberId(memberId)
+                .email(memberEntity.getEmail())
+                .nickname(memberEntity.getNickname())
+                .introduction(memberEntity.getIntroduction())
+                .profileImageUrl(memberEntity.getProfileImageUrl())
+                .mannerScore(memberEntity.getMannerScore())
+                .mannerScoreCount(memberEntity.getMannerScoreCount())
+                .addressDepth1Name(mainAddress.getAddressDepth1().getName())
+                .addressDepth2Name(mainAddress.getAddressDepth2().getName())
+                .positions(positions)
+                .joinedCrews(crewReader.readAllConfirmedByMemberId(memberId))
+                .joinedGames(joinedGames)
                 .build();
     }
 }
