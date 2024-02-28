9e4ac01265d4cf7afce180667e89086b349735f2
kylekim2123 <kyle2123@naver.com>
2024-02-28 19:36:44 +0900

    refactor: Chat 도메인 구현계층 관련 폴더 및 파일 생성

---
+++ src/main/java/kr/pickple/back/chat/implement/ChatMapper.java
@@ -0,0 +1,9 @@
+package kr.pickple.back.chat.implement;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ChatMapper {
+
+}
---
+++ src/main/java/kr/pickple/back/chat/implement/ChatWriter.java
@@ -0,0 +1,11 @@
+package kr.pickple.back.chat.implement;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ChatWriter {
+
+}
