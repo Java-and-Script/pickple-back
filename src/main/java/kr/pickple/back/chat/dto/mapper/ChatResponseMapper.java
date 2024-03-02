793b98f46723b2beb8dcb6511347c49bb71daae0
kylekim2123 <kyle2123@naver.com>
2024-03-02 18:43:59 +0900

    refactor: 채팅 관련 DTO 매퍼 별도 생성

---
+++ src/main/java/kr/pickple/back/chat/dto/mapper/ChatResponseMapper.java
@@ -0,0 +1,40 @@
+package kr.pickple.back.chat.dto.mapper;
+
+import java.util.List;
+
+import kr.pickple.back.chat.domain.ChatRoomDomain;
+import kr.pickple.back.chat.dto.response.ChatMemberResponse;
+import kr.pickple.back.chat.dto.response.ChatRoomDetailResponse;
+import kr.pickple.back.member.domain.MemberDomain;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ChatResponseMapper {
+
+    public static ChatRoomDetailResponse mapToPersonalChatRoomDetailResponseDto(
+            final MemberDomain sender,
+            final MemberDomain receiver,
+            final ChatRoomDomain chatRoom
+    ) {
+        return ChatRoomDetailResponse.builder()
+                .id(chatRoom.getChatRoomId())
+                .roomName(receiver.getNickname())
+                .roomIconImageUrl(receiver.getProfileImageUrl())
+                .type(chatRoom.getType())
+                .domainId(receiver.getMemberId())
+                .memberCount(chatRoom.getMemberCount())
+                .maxMemberCount(chatRoom.getMaxMemberCount())
+                .members(List.of(mapToChatMemberResponseDto(sender), mapToChatMemberResponseDto(receiver)))
+                .createdAt(chatRoom.getCreatedAt())
+                .build();
+    }
+
+    public static ChatMemberResponse mapToChatMemberResponseDto(final MemberDomain member) {
+        return ChatMemberResponse.builder()
+                .id(member.getMemberId())
+                .nickname(member.getNickname())
+                .profileImageUrl(member.getProfileImageUrl())
+                .build();
+    }
+}
