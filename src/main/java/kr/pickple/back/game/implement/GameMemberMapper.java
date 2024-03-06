a863b5b0a5d364c0ca72d4284f4b46d3d8ccdb3a
이현호 <tiger0192004@gmail.com>
2024-03-06 23:28:41 +0900

    refactor: 게임멤버 등록, 조회, 상태 수정, 삭제 기능을 변화된 설계에 맞추어 수정

--- src/main/java/kr/pickple/back/game/implement/GameMemberMapper.java
+++ src/main/java/kr/pickple/back/game/implement/GameMemberMapper.java
@@ -1,22 +1,22 @@
 package kr.pickple.back.game.implement;
 
 import kr.pickple.back.game.domain.GameDomain;
-import kr.pickple.back.game.domain.GameMember;
+import kr.pickple.back.game.domain.GameMemberDomain;
 import kr.pickple.back.game.repository.entity.GameMemberEntity;
 import kr.pickple.back.member.domain.MemberDomain;
 
 public class GameMemberMapper {
 
-    public static GameMemberEntity mapGameMemberDomainToEntity(final GameMember gameMember) {
+    public static GameMemberEntity mapGameMemberDomainToEntity(final GameMemberDomain gameMemberDomain) {
         return GameMemberEntity.builder()
-                .status(gameMember.getStatus())
-                .memberId(gameMember.getMember().getMemberId())
-                .gameId(gameMember.getGame().getGameId())
+                .status(gameMemberDomain.getStatus())
+                .memberId(gameMemberDomain.getMember().getMemberId())
+                .gameId(gameMemberDomain.getGame().getGameId())
                 .build();
     }
 
-    public static GameMember mapGameMemberEntityToDomain(GameMemberEntity gameMemberEntity, MemberDomain memberDomain, GameDomain gameDomain) {
-        return GameMember.builder()
+    public static GameMemberDomain mapGameMemberEntityToDomain(GameMemberEntity gameMemberEntity, MemberDomain memberDomain, GameDomain gameDomain) {
+        return GameMemberDomain.builder()
                 .gameMemberId(gameMemberEntity.getId())
                 .status(gameMemberEntity.getStatus())
                 .member(memberDomain)
