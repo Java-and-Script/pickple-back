8d3d7a31c6f7df9b9c1dbf97ecabdcf37517dfda
이현호 <tiger0192004@gmail.com>
2024-03-06 16:51:32 +0900

    refactor: GameFacadeService, RedisExpirationListener 내 타 서비스 의존성 제거, 구현 계층으로 대체

--- src/main/java/kr/pickple/back/game/service/GameFacadeService.java
+++ src/main/java/kr/pickple/back/game/service/GameFacadeService.java
@@ -1,13 +1,18 @@
 package kr.pickple.back.game.service;
 
+import static kr.pickple.back.common.domain.RegistrationStatus.*;
+
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 import kr.pickple.back.address.dto.response.MainAddress;
 import kr.pickple.back.address.implement.AddressReader;
+import kr.pickple.back.game.domain.GameDomain;
+import kr.pickple.back.game.dto.mapper.GameResponseMapper;
 import kr.pickple.back.game.dto.response.GameResponse;
 import kr.pickple.back.game.dto.response.GamesAndLocationResponse;
+import kr.pickple.back.game.implement.GameReader;
 import kr.pickple.back.map.domain.MapPolygon;
 import kr.pickple.back.map.service.MapService;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +22,21 @@ import lombok.RequiredArgsConstructor;
 public class GameFacadeService {
 
     private final AddressReader addressReader;
-
-    private final GameService gameService;
     private final MapService mapService;
+    private final GameReader gameReader;
 
     public GamesAndLocationResponse findGamesWithInAddress(final String addressDepth1, final String addressDepth2) {
         final MainAddress mainAddress = addressReader.readMainAddressByNames(addressDepth1, addressDepth2);
-        final List<GameResponse> gameResponses = gameService.findGamesWithInAddress(mainAddress);
+
+        final List<GameDomain> gameDomains = gameReader.findGamesWithInAddress(mainAddress);
+        final List<GameResponse> gameResponses = gameDomains.stream()
+                .map(gameDomain -> GameResponseMapper.mapToGameResponseDto(
+                                gameDomain,
+                                gameReader.readAllMembersByGameIdAndStatus(gameDomain.getGameId(), CONFIRMED)
+                        )
+                )
+                .toList();
+
         final MapPolygon mapPolygon = mapService.findMapPolygonByMainAddress(mainAddress);
 
         return GamesAndLocationResponse.of(gameResponses, mapPolygon);
