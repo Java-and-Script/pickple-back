396ab2e271c9ba8ad6eede6ee72ff54b25ce197a
이현호 <tiger0192004@gmail.com>
2024-03-06 10:37:07 +0900

    feat: 게임 repository에 게임의 멤버 수와 모집 상태 변경하는 기능 추가

---
+++ src/main/java/kr/pickple/back/game/implement/GameMapper.java
@@ -0,0 +1,89 @@
+package kr.pickple.back.game.implement;
+
+import java.util.List;
+
+import org.locationtech.jts.geom.Point;
+
+import kr.pickple.back.address.dto.response.MainAddress;
+import kr.pickple.back.chat.domain.ChatRoom;
+import kr.pickple.back.game.domain.GameDomain;
+import kr.pickple.back.game.domain.NewGame;
+import kr.pickple.back.game.repository.entity.GameEntity;
+import kr.pickple.back.game.repository.entity.GamePosition;
+import kr.pickple.back.member.domain.MemberDomain;
+import kr.pickple.back.member.domain.MemberPosition;
+import kr.pickple.back.position.domain.Position;
+
+public class GameMapper {
+
+    public static GameDomain mapToGameDomain(
+            final GameEntity gameEntity,
+            final MainAddress mainAddress,
+            final MemberDomain host,
+            final ChatRoom chatRoom,
+            final List<Position> positions
+    ) {
+        return GameDomain.builder()
+                .gameId(gameEntity.getId())
+                .content(gameEntity.getContent())
+                .playDate(gameEntity.getPlayDate())
+                .playStartTime(gameEntity.getPlayStartTime())
+                .playEndTime(gameEntity.getPlayEndTime())
+                .playTimeMinutes(gameEntity.getPlayTimeMinutes())
+                .mainAddress(gameEntity.getMainAddress())
+                .detailAddress(gameEntity.getDetailAddress())
+                .latitude(gameEntity.getPoint().getY())
+                .longitude(gameEntity.getPoint().getX())
+                .status(gameEntity.getStatus())
+                .viewCount(gameEntity.getViewCount())
+                .cost(gameEntity.getCost())
+                .memberCount(gameEntity.getMemberCount())
+                .maxMemberCount(gameEntity.getMaxMemberCount())
+                .host(host)
+                .addressDepth1(mainAddress.getAddressDepth1().getName())
+                .addressDepth2(mainAddress.getAddressDepth2().getName())
+                .positions(positions)
+                .chatRoom(chatRoom)
+                .build();
+    }
+
+    public static GameEntity mapNewGameDomainToEntity(NewGame newGame, Point point, MainAddress mainAddress) {
+        return GameEntity.builder()
+                .content(newGame.getContent())
+                .playDate(newGame.getPlayDate())
+                .playStartTime(newGame.getPlayStartTime())
+                .playEndTime(newGame.getPlayEndTime())
+                .playTimeMinutes(newGame.getPlayTimeMinutes())
+                .mainAddress(newGame.getMainAddress())
+                .detailAddress(newGame.getDetailAddress())
+                .cost(newGame.getCost())
+                .maxMemberCount(newGame.getMaxMemberCount())
+                .hostId(newGame.getHost().getMemberId())
+                .point(point)
+                .addressDepth1Id(mainAddress.getAddressDepth1().getId())
+                .addressDepth2Id(mainAddress.getAddressDepth2().getId())
+                .chatRoomId(newGame.getChatRoom().getId())
+                .build();
+    }
+
+    public static List<GamePosition> mapToGamePositionEntities(final List<Position> positions, final Long gameId) {
+        return positions.stream()
+                .map(position -> GamePosition.builder()
+                        .gameId(gameId)
+                        .position(position)
+                        .build()
+                ).toList();
+    }
+
+    public static List<MemberPosition> mapToMemberPositionEntities(
+            final List<Position> positions,
+            final Long memberId
+    ) {
+        return positions.stream()
+                .map(position -> MemberPosition.builder()
+                        .memberId(memberId)
+                        .position(position)
+                        .build()
+                ).toList();
+    }
+}
